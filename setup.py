ce4d745c9d8387c9d7c52883e2756bc58fda4268
root <root@k8s-crawler-master-1.nferxops.net>
2022-05-20 12:28:47 +0000

    README for using older version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 version = "0.2.0"
 setup(
     name="diskarray",
-    python_requires=">3.8.0",
+    python_requires=">=3.8.0",
     version=version,
     description="A resizable and readable numpy array on disk",
     keywords="diskarray",
